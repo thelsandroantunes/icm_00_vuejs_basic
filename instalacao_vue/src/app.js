46e08bc322911c25e21057366f9c2754b554be14
Thelsandro <tca.eng17@uea.edu.br>
2022-03-24 13:33:05 -0400

    part_00
    introdução ao Vue.

---
+++ instalacao_vue/src/app.js
@@ -0,0 +1,20 @@
+const MyNameApp = {
+  data(){
+    return {
+      name : "",
+      idade: "",
+      input_name: ""
+    }
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+
+      console.log(this.input_name);
+
+      this.name = this.input_name;
+    }
+  }
+}
+
+Vue.createApp(MyNameApp).mount("#app");

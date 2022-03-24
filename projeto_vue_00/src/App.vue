9646618b4d0eadba0346fee610bc94951f773a2b
Thelsandro <tca.eng17@uea.edu.br>
2022-03-24 15:31:30 -0400

    part_01
    manualmente

---
+++ projeto_vue_00/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <PrimeiroComponente />
+  <LifeCycle />
+  <Pessoa />
+</div>
+</template>
+
+<script>
+
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
+import Pessoa from './components/Pessoa.vue'
+export default{
+
+  name: 'App',
+  components: {
+    PrimeiroComponente,
+    LifeCycle,
+    Pessoa
+  }
+}
+
+</script>
---
+++ projeto_vue_00/src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1> Meu nome é: {{nome}}</h1>
+</template>
+
+<script>
+export default{
+  name:'LifeCycle',
+  data(){
+    return {
+      nome: 'Ainda não sei'
+    }
+  },
+  created(){
+    setTimeout(() => {
+      this.nome = "Thelsandro"
+    }, 1000)
+
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.nome = "Thayná"
+    }, 2000)
+
+  }
+}
+</script>
